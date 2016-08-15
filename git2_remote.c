1ea079d16fcd8c67a4ac64ea837163244299d1e7
Mark Karpeles <karpelesm@gmail.com>
2016-08-15 10:40:28 +0900

    remote_connect

--- git2_remote.c
+++ git2_remote.c
@@ -42,7 +42,6 @@ static PHP_METHOD(Remote, create_anonymous) {
 	}
 }
 
-
 #define GIT2_REMOTE_FETCH() git2_remote_object_t *intern = (git2_remote_object_t*)Z_OBJ_P(getThis()); \
 	if (intern->remote == NULL) { \
 		zend_throw_exception(zend_exception_get_default(TSRMLS_C), "Git2\\Remote object in invalid state", 0 TSRMLS_CC); \
@@ -71,6 +70,29 @@ GIT2_REMOTE_GET_STRING(name)
 GIT2_REMOTE_GET_STRING(url)
 GIT2_REMOTE_GET_STRING(pushurl)
 GIT2_REMOTE_GET_LONG(refspec_count)
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_remote_connect, 0, 0, 1)
+	ZEND_ARG_INFO(0, is_push) /* set to true for push, false for fetch */
+ZEND_END_ARG_INFO()
+
+static PHP_METHOD(Remote, connect) {
+	zend_bool is_push;
+
+	GIT2_REMOTE_FETCH();
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "b", &is_push) == FAILURE) {
+		return;
+	}
+
+	// TODO handle callbacks
+	int res = git_remote_connect(intern->remote, is_push ? GIT_DIRECTION_PUSH : GIT_DIRECTION_FETCH, NULL);
+
+	if (res == 0) {
+		RETURN_TRUE;
+	}
+
+	RETURN_FALSE;
+}
 
 void git2_remote_spawn(zval **return_value, git_remote *remote TSRMLS_DC) {
 	git2_remote_object_t *intern;
@@ -107,12 +129,15 @@ static void php_git2_remote_free_object(zend_object *object TSRMLS_DC) {
 	// no need with PHP7 to free intern
 }
 
+#define PHP_GIT2_REMOTE_ME_P(_x) PHP_ME(Remote, _x, arginfo_remote_##_x, ZEND_ACC_PUBLIC)
+
 static zend_function_entry git2_remote_methods[] = {
 	PHP_ME(Remote, create_anonymous, arginfo_remote_create_anonymous, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
 	PHP_ME(Remote, name, arginfo_remote_name, ZEND_ACC_PUBLIC)
 	PHP_ME(Remote, url, arginfo_remote_url, ZEND_ACC_PUBLIC)
 	PHP_ME(Remote, pushurl, arginfo_remote_pushurl, ZEND_ACC_PUBLIC)
 	PHP_ME(Remote, refspec_count, arginfo_remote_refspec_count, ZEND_ACC_PUBLIC)
+	PHP_GIT2_REMOTE_ME_P(connect)
 /*	PHP_ME(Remote, __construct, arginfo___construct, ZEND_ACC_PUBLIC) */
 	{ NULL, NULL, NULL }
 };
